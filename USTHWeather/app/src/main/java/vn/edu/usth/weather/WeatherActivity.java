4c187d32783d4138e0bbab9eb97b05170a969a1f
Vo Chi Dat <dat.usth2000@gmail.com>
2021-10-08 01:02:54 +0700

    Practical work 3

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -11,6 +11,8 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        ForecastFragment firstFragment = new ForecastFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
     }
     @Override
     protected void onStart(){
@@ -41,5 +43,4 @@ public class WeatherActivity extends AppCompatActivity {
         super.onDestroy();
         Log.i("Weather", "In onDestroy");
     }
-
 }
